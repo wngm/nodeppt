77940c088f8e550ee3d25a71e9dca4404c621cda
wng <404313843@qq.com>
2017-06-26 20:33:30 +0800

    哎···有点复杂

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-var nodeppt=require('nodeppt')
-var commander=require('commander')
-console.log(nodeppt)
-console.log(commander)
+var nodeppt=require('nodeppt');
+var commander=require('commander');
+var server=require('./lib/server');
+
+
+var argvObj = {
+    dir:'./md_lib/',
+    port:'8080',
+    controller:false,
+    watch:'',
+    host:'192.168.31.144',
+
+};
+server.start(8080,__dirname+'/md_lib/',argvObj.host,argvObj)
